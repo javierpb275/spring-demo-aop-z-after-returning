5d7d74437ab52d8fc16fddc55416e851687d4baa
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-07-22 20:13:03 +0200

    called findAccounts() method to find the accounts and displayed the accounts

--- src/com/javi/aopdemo/AfterReturningDemoApp.java
+++ src/com/javi/aopdemo/AfterReturningDemoApp.java
@@ -1,5 +1,7 @@
 package com.javi.aopdemo;
 
+import java.util.List;
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.javi.aopdemo.dao.AccountDAO;
@@ -9,13 +11,25 @@ public class AfterReturningDemoApp {
 
 	public static void main(String[] args) {
 		
-		
 		// read spring config java class
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
 		
 		
 		// get the bean from spring container
 		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+		
+		
+		// call method to find the accounts
+		List<Account> theAccounts = theAccountDAO.findAccounts();
+		
+		
+		// display the accounts
+		System.out.println("\n\nMain Program: AfterReturningDemoApp");
+		System.out.println("----");
+		
+		System.out.println(theAccounts);
+		
+		System.out.println("\n");
 		
 		
 		// close the context
