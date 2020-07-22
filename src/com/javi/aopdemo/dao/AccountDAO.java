ed6ff8842743b6f33ed8bfbc1db256cbb674adf1
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-07-22 19:27:16 +0200

    first commit

---
+++ src/com/javi/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,50 @@
+package com.javi.aopdemo.dao;
+
+import org.springframework.stereotype.Component;
+
+import com.javi.aopdemo.Account;
+
+@Component
+public class AccountDAO {
+	
+	private String name;
+	private String serviceCode;
+	
+	public void addAccount(Account theAccount, boolean vipFlag) {
+		
+		System.out.println(getClass() + ": DOING MY DB WORK: ADDING AN ACCOUNT");
+		
+	}
+	
+	public boolean doWork() {
+		
+		System.out.println(getClass() + ": doWork");
+		return false;
+		
+	}
+	
+	//getters and setters
+
+	public String getName() {
+		System.out.println(getClass() + ": in getName()");
+		return name;
+	}
+
+	public void setName(String name) {
+		System.out.println(getClass() + ": in setName()");
+		this.name = name;
+	}
+
+	public String getServiceCode() {
+		System.out.println(getClass() + ": in getServiceCode");
+		return serviceCode;
+	}
+
+	public void setServiceCode(String serviceCode) {
+		System.out.println(getClass() + ": in setServiceCode");
+		this.serviceCode = serviceCode;
+	}
+	
+	
+
+}
